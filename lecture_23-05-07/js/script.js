03d54a21f49b6c5dda77e02bd48a2a32e0105a3d
Ivan Shablii <ivan182sh@gmail.com>
2023-05-11 12:25:58 +0300

    addresed mentor review comments

--- lecture_23-05-07/js/script.js
+++ lecture_23-05-07/js/script.js
@@ -1,33 +1,44 @@
-// ----- let's get all text from all nodes
-const description = document.getElementsByClassName("playlist__description")[0]
-  .children;
+// --STEP-1-- let's get all text from all nodes
 
-let allText = [];
-for (i = 0; i < description.length; ++i) {
-  allText += description[i].innerText;
-}
+const description = Array.from(
+  document.getElementsByClassName("playlist__description")[0].children
+);
 
-// ----- let's convert text into array of separate words
+let allText = description.map((node) => node.innerText).toString();
+
+// --STEP-2-- let's convert text into array of separate words
+
 let allWords = allText.match(/\b(\w+)\b/g);
 
-// ----- let's create array of words, that are used at least twice
+// --STEP-3-- let's create array of words, that are used at least twice
+
 let wordsToRecolor = allWords.filter(
   (value, index, self) => self.indexOf(value) === index
 );
 
-// ----- let's make this words red
-for (i = 0; i < description.length; ++i) {
-  let arr = description[i].innerHTML;
-  for (j = 0; j < wordsToRecolor.length; ++j) {
-    let word = wordsToRecolor[j];
-    let wordRegex = new RegExp("\\b" + word + "\\b", "g"); // to replace whole words only, not part of it
-    if (arr.includes(word)) {
-      arr = arr.replace(
-        wordRegex,
+// --STEP-4-- create function to convert word to regex
+function toRegexp(word) {
+  return new RegExp("\\b" + word + "\\b", "g");
+}
+// --STEP-5-- create function to recolor words in array
+function recolorString(str) {
+  for (i = 0; i < wordsToRecolor.length; ++i) {
+    let word = wordsToRecolor[i];
+    if (str.includes(word)) {
+      str = str.replace(
+        toRegexp(word), // слово, яке замінимо. Без регексп збіги в частині слова теж замінить
         `<span style="color:red">` + word + `</span>`
       );
-    } else {
-      description[i].innerHTML = arr;
     }
   }
+  return str;
 }
+
+// --STEP-6-1- get innerHTML from nodes and store it in arr
+let arr = description.map((node) => node.innerHTML);
+
+// --STEP-6-2- recolor each word in each node if it repeats at least once
+let newArr = arr.map((node) => recolorString(node));
+
+// --STEP-6-2- replace text in nodes
+description.map((node, index) => (node.innerHTML = newArr[index]));
